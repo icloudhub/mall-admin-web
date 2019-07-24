e660e96fec7e955c6c8bcba52af31be56986c9fc
静静地白色外套 <296019487@qq.com>
2019-07-24 18:50:09 +0800

    添加用户列表

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -7,9 +7,9 @@
                ref="loginForm"
                label-position="left">
         <div style="text-align: center">
-          <svg-icon icon-class="login-mall" style="width: 56px;height: 56px;color: #409EFF"></svg-icon>
+          <svg-icon icon-class="login-mall" style="width: 56px;height: 56px;color: #409EF"></svg-icon>
         </div>
-        <h2 class="login-title color-main">mall-admin-web</h2>
+        <h2 class="login-title color-main">后台管理系统</h2>
         <el-form-item prop="username">
           <el-input name="username"
                     type="text"
@@ -44,16 +44,6 @@
       </el-form>
     </el-card>
     <img :src="login_center_bg" class="login-center-layout">
-    <el-dialog
-      title="特别赞助"
-      :visible.sync="dialogVisible"
-      width="30%">
-      <span>mall项目已由CODING特别赞助，点击去支持，页面加载完后点击<span class="color-main font-medium">免费体验</span>按钮即可完成支持，谢谢！</span>
-      <span slot="footer" class="dialog-footer">
-    <el-button @click="dialogCancel">残忍拒绝</el-button>
-    <el-button type="primary" @click="dialogConfirm">去支持</el-button>
-  </span>
-    </el-dialog>
   </div>
 </template>
 
@@ -90,8 +80,7 @@
         },
         loading: false,
         pwdType: 'password',
-        login_center_bg,
-        dialogVisible:false
+        login_center_bg
       }
     },
     methods: {
@@ -105,11 +94,7 @@
       handleLogin() {
         this.$refs.loginForm.validate(valid => {
           if (valid) {
-            let isSupport = getSupport();
-            if(isSupport===undefined||isSupport==null){
-              this.dialogVisible =true;
-              return;
-            }
+           
             this.loading = true;
             this.$store.dispatch('Login', this.loginForm).then(() => {
               this.loading = false;
@@ -122,15 +107,6 @@
             return false
           }
         })
-      },
-      dialogConfirm(){
-        this.dialogVisible =false;
-        setSupport(true);
-        window.location.href=SupportUrl;
-      },
-      dialogCancel(){
-        this.dialogVisible = false;
-        setSupport(false);
       }
     }
   }
